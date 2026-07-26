aa5d7f6bb15a06113ddbc117ec399d765b557bac
pavanisai2828 <pavani.sai1313@gmailcom>
2026-07-27 02:50:46 +0530

    second code added

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  ami_id= data.aws_ami.join_devops
+  ami_id= data.aws_ami.join_devops.id
   bastion_sg_id= data.aws_ssm_parameter.bastion_sg_id
   public_subnet_id=split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
 
